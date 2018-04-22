027de78beedb5be6e70777d9febaf52cf657c2a7
nicetoseeyou <hello4world@yeah.net>
2018-04-22 13:49:54 +0800

    Init Maven

---
+++ src/main/java/lab/quantum/kevin/App.java
@@ -0,0 +1,13 @@
+package lab.quantum.kevin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
